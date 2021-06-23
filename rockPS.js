6f4334714e57aa8ededbf83f93edee6d849e3eda
Christina L <65386414+christina-ml@users.noreply.github.com>
2021-06-23 17:04:01 -0400

    Update rockPS.js
    add playGame function

--- rockPS.js
+++ rockPS.js
@@ -1,8 +1,3 @@
-// define variables
-// const rock = 'rock';
-// const paper = 'paper';
-// const scissors = 'scissors';
-
 /*
 use arrow function syntax, to create a function that takes a single parameter
 */
@@ -13,7 +8,8 @@ const getUserChoice = userInput => {
   userInput = userInput.toLowerCase();
 
   // if..else statement for rock, paper, OR scissors
-  if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+  // add secret 'bomb' cheat code
+  if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors' || userInput === 'bomb') {
     return userInput;
   } else {
     console.log('error! - this isn\'t a valid choice.');
@@ -31,7 +27,7 @@ const getComputerChoice = () => {
   /*
   Define variable & create a random whole number between 0 and 2
   */
-const randomNumber = Math.floor(Math.random() * 3);
+  let randomNumber = Math.floor(Math.random() * 3);
   /*
   use an if..else or switch statement to return the computer's choice.
   Switch statement:
@@ -47,13 +43,19 @@ const randomNumber = Math.floor(Math.random() * 3);
 };
 
 // test the function by calling it
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
+
 
 // Determine a winner!
 /*
 create function that takes 2 parameters
 */
 const determineWinner = (userChoice, computerChoice) => {
+// use if...else statement for secret cheat code 'bomb'
+if (userChoice === 'bomb') {
+  return 'You cheated and won!';
+}
+
   // use if..else statement for a tie
 if (userChoice === computerChoice) {
   return 'The game is a tie!';
@@ -88,6 +90,32 @@ if (userChoice === 'scissors') {
 }
 
 // test your function!
-console.log(determineWinner('paper', 'scissors')); // prints something like 'The computer won!'
-console.log(determineWinner('paper', 'paper')); // prints something like 'The game is a tie!'
-console.log(determineWinner('paper', 'rock')); // prints something like 'The user won!'
+console.log(determineWinner('paper', 'scissors'));
+console.log(determineWinner('paper', 'paper'));
+console.log(determineWinner('paper', 'rock'));
+};
+
+/*
+Everything's set up! Start the game and log the results.
+*/
+// create a function named playGame
+const playGame = () => {
+  /*create variable for user choice
+change this variable to change the user's choice. Try secret cheat code: 'bomb' to win!
+  */
+  const userChoice = getUserChoice('rock');
+
+  // create a variable for cpu choice
+  const computerChoice = getComputerChoice();
+
+  // print variables to console
+  console.log('You threw: ' + userChoice);
+  console.log('Computer threw: ' + computerChoice);
+
+  // call determine winner function
+  console.log(determineWinner(userChoice, computerChoice));
+
+};
+
+// call function
+playGame();
